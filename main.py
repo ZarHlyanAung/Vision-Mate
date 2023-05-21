a7daf0859bc3758e0fd644b69cdb406ebf13b14c
Zar Hlyan Aung <127838777+ZarHlyanAung@users.noreply.github.com>
2023-05-21 08:43:14 +0630

    Update main.py

--- main.py
+++ main.py
@@ -10,34 +10,35 @@ def loadModel():
     model = tf.keras.models.load_model("vision-mate-8.h5")
     return model
 
-
 model = loadModel()
 
 # Define the Streamlit app
 st.title('Vision Mate Demo')
-# st.sidebar.title('Upload Image')
-    
-# Create a file uploader in the sidebar
+
+# Create a file uploader
 uploaded_file = st.file_uploader('Upload an image of a Myanmar bank note', type=['jpg', 'jpeg', 'png'])
-    
+
 if uploaded_file is not None:
     # Read the uploaded image
     image = Image.open(uploaded_file)
-        
-    # Display the uploaded image
-    st.image(image, caption='Uploaded Image', use_column_width=True)
-        
+
+    # Convert image to grayscale
+    grayscale_image = image.convert('L')
+
+    # Display the uploaded grayscale image
+    st.image(grayscale_image, caption='Grayscale Image', use_column_width=True)
+
     # Preprocess the image for model input
-    image = image.resize((100, 100)) # Resize the image to match the input size of the model
-    image = np.array(image) # Convert the image to a numpy array
-    image = image / 255.0 # Normalize the pixel values to [0, 1]
-    image = np.expand_dims(image, axis=0) # Add an extra dimension for batch size
-        
+    resized_image = grayscale_image.resize((100, 100))  # Resize the image to match the input size of the model
+    image_array = np.array(resized_image)  # Convert the image to a numpy array
+    normalized_image = image_array / 255.0  # Normalize the pixel values to [0, 1]
+    input_image = np.expand_dims(normalized_image, axis=0)  # Add an extra dimension for batch size
+
     # Make predictions with the model
-    prediction = model.predict(image)
+    prediction = model.predict(input_image)
     predicted_label_index = np.argmax(prediction[0])
     predicted_label = class_labels[predicted_label_index]
-        
+
     # Display the predicted label
     st.success(predicted_label)
     st.write(prediction[0][predicted_label_index])
