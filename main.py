213edd7f0778f826dd9cbbfff7b0e00a609c3b65
Zar Hlyan Aung <zarhlyanaung.ai@gmail.com>
2023-05-31 23:21:07 +0630

    v12 is ready

--- main.py
+++ main.py
@@ -6,9 +6,11 @@ import tensorflow as tf
 # Define the class labels
 class_labels = ["100", "1000", "10000", "200", "50", "500", "5000", "other"]
 
+
 def loadModel():
-    model = tf.keras.models.load_model("vision-mate-10.h5")
+    model = tf.keras.models.load_model("vision-mate-12.h5")
     return model
+
 
 model = loadModel()
 
@@ -16,7 +18,8 @@ model = loadModel()
 st.title('Vision Mate Demo')
 
 # Create a file uploader
-uploaded_file = st.file_uploader('Upload an image of a Myanmar bank note', type=['jpg', 'jpeg', 'png'])
+uploaded_file = st.file_uploader(
+    'Upload an image of a Myanmar bank note', type=['jpg', 'jpeg', 'png'])
 
 if uploaded_file is not None:
     # Read the uploaded image
@@ -29,10 +32,13 @@ if uploaded_file is not None:
     st.image(grayscale_image, caption='Grayscale Image', use_column_width=True)
 
     # Preprocess the image for model input
-    resized_image = grayscale_image.resize((100, 100))  # Resize the image to match the input size of the model
+    # Resize the image to match the input size of the model
+    resized_image = grayscale_image.resize((100, 100))
     image_array = np.array(resized_image)  # Convert the image to a numpy array
-    normalized_image = image_array / 255.0  # Normalize the pixel values to [0, 1]
-    input_image = np.expand_dims(normalized_image, axis=0)  # Add an extra dimension for batch size
+    # Normalize the pixel values to [0, 1]
+    normalized_image = image_array / 255.0
+    # Add an extra dimension for batch size
+    input_image = np.expand_dims(normalized_image, axis=0)
 
     # Make predictions with the model
     prediction = model.predict(input_image)
